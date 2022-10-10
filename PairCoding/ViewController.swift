43ffe0348d55bf03803c0f3da954655331eb0e08
Vlad Korshko <vladkorshko@gmail.com>
2022-10-10 12:45:34 -0400

    first commit

---
+++ PairCoding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PairCoding
+//
+//  Created by Vlad Korshko on 2022-10-10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
